8a5bc9694ea87ab9ac20767589b24cabcf36558b
godtyze <omgfail301@gmail.com>
2022-01-22 23:19:52 +0600

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const config = new Map(bracketsConfig);
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === config.get(stack[stack.length - 1])) {
+            stack.pop();
+        } else {
+            stack.push(str[i]);
+        }
+    }
+
+    return stack.length === 0;
 }
